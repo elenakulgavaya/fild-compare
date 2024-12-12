71dad48aba5e9552f31dfd1a9c2c08796b2977d3
Elena Kulgavaya <elena.kulgavaya@gmail.com>
2024-12-12 10:31:03 +0100

    Tests

--- tests/test_rules.py
+++ tests/test_rules.py
@@ -6,7 +6,9 @@ from fild.sdk.fakeable import Fakeable, fake_string_attr
 
 from fild_compare import compare_data
 from fild_compare.rules import (
-    dates_equal_with_delta_3s, equal_with_accuracy_4, formatted_dates_equal_with_delta, has_new_value, has_some_value, is_valid_uuid, sorted_lists_equal, timestamp_equal_with_delta_5s
+    dates_equal_with_delta_3s, equal_with_accuracy_4,
+    formatted_dates_equal_with_delta, has_new_value, has_some_value,
+    is_valid_uuid, sorted_lists_equal, timestamp_equal_with_delta_5s
 )
 
 
